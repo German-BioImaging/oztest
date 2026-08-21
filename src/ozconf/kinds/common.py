90be81d0e939f001459dd70a235e7b26626405dc
Chris Barnes <chris.barnes@gerbi-gmb.de>
2026-08-21 18:14:33 +0100

    ozconf check: add --output --format
    Return results as JSON, and optionally to a file.

--- src/ozconf/kinds/common.py
+++ src/ozconf/kinds/common.py
@@ -1,11 +1,19 @@
 from __future__ import annotations
 
-from typing import Literal, NamedTuple
+import logging
+import sys
+from collections.abc import Generator
+from contextlib import contextmanager
+from dataclasses import dataclass
+from pathlib import Path
+from typing import IO, Literal, NamedTuple
 
 from ..case_filter import Case
 
 JSON = int | float | str | None | list["JSON"] | dict[str, "JSON"]
 Status = Literal["pass", "fail", "error", "skip"]
+
+logger = logging.getLogger(__name__)
 
 
 def format_status(s: Status) -> str:
@@ -27,8 +35,31 @@ def format_status(s: Status) -> str:
 
 
 class Result(NamedTuple):
+    """Class describing the information returned by a dingus."""
+
     case: Case
     invocation: list[str]
     return_code: int
     output: JSON | None
     stderr: str | None
+
+
+@dataclass
+class OutputConfig:
+    out_file: Path | None
+    format: Literal["tsv", "json"]
+
+    def is_a_tty(self) -> bool:
+        return self.out_file is None and sys.stdout.isatty()
+
+    @contextmanager
+    def open(self) -> Generator[IO[str]]:
+        if self.out_file is None:
+            yield sys.stdout
+            return
+
+        if self.out_file.exists():
+            logger.warning("Overwriting output file %s", self.out_file)
+
+        with open(self.out_file, "w") as f:
+            yield f
